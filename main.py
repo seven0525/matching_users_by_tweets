67f21888838cfc206ed0d93b41119af0e383ba37
mimizuk <mizuku3k@gmail.com>
2018-09-12 23:24:25 +0900

    Update main.py main.ipynb

--- main.py
+++ main.py
@@ -121,10 +121,42 @@ for user in users:
     shaped_user_tweets = get_shaped_tweets(get_user_tweets(user))
     tweets_trans_json(shaped_user_tweets, user)
     if exists('./' + file_name)  == False:
-        get_insights_analytics(user)
-        with codecs.open(file_name, 'w', 'utf-8') as fw:
-            json.dump(get_big5(user), fw, indent=2)
+#     get_insights_analytics(user)
+#     with codecs.open(file_name, 'w', 'utf-8') as fw:
+#         json.dump(get_big5(user), fw, indent=2)
     print(get_big5(user))
 
 # print('取得ツイート数: ', len(shaped_user_tweets))
+
+
+# 以下、上記で受け取ったbig5\-\[user\].jsonに関して操作を行う(P.I.の使用回数の節約のため)
+
+# In[ ]:
+
+
+users = ['fu_wo_msk', 'mi_so_ka']
+user_and_big5 = {}
+
+# big5データ整形(後ほど上の項に組み込む)
+for user in users:
+    file_name = 'big5-' + user +'.json'
+    shaped_data = {}
+    with open('./' + file_name, 'r') as fr:
+        data_list = json.load(fr)
+        for data in data_list:
+            shaped_data[data['name']] = data['raw_score'] 
+    user_and_big5[user] = shaped_data 
+    
+# big5の生の値の差を取り出す
+def get_big5_diff(data):
+    open_diff = data[users[0]]['Openness'] - data[users[1]]['Openness']
+    cons_diff = data[users[0]]['Conscientiousness'] - data[users[1]]['Conscientiousness']
+    print(open_diff)
+    print(cons_diff)
+
+            
+            
+
+print(user_and_big5)
+get_big5_diff(user_and_big5)
 
