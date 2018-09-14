77d0d7ee7bb95793b5e5f7c0afe769bb60ead1ee
mimizukmsk <enone999@gmail.com>
2018-09-14 22:12:51 +0900

    Rename config.py for config-sample.py

---
+++ config-sample.py
@@ -0,0 +1,27 @@
+# Twitter API
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+
+# Watson API
+USER_NAME = ""
+PASSWORD = ""
+
+def get_consumer_key():
+    return CONSUMER_KEY
+
+def get_consumer_secret():
+    return CONSUMER_SECRET
+
+def get_access_token():
+    return ACCESS_TOKEN
+
+def get_access_secret():
+    return ACCESS_SECRET
+
+def get_username():
+    return USER_NAME
+
+def get_password():
+    return PASSWORD
